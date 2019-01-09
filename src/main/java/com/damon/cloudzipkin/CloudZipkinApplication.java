29abfe7cb030814f7de5947446b71de90bb1230c
damon <cuilei.cosmo@haier.com>
2019-01-09 18:44:45 +0800

    a

--- src/main/java/com/damon/cloudzipkin/CloudZipkinApplication.java
+++ src/main/java/com/damon/cloudzipkin/CloudZipkinApplication.java
@@ -3,11 +3,13 @@ package com.damon.cloudzipkin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import zipkin.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZipkinServer
+@RefreshScope
 public class CloudZipkinApplication {
 
     public static void main(String[] args) {
